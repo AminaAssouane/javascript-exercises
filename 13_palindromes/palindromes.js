8697f0f882f1761cd053e4619a8b34287433dc4c
Amina <minassouane@gmail.com>
2025-05-22 06:45:28 +0100

    Implement my (zwrong) solution. Fails 2 tests.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+function isAlphaNumeric(char) {
+  return /[a-zA-Z0-9]/.test(char);
+}
+const palindromes = function (str) {
+  let i = 0;
+  let j = str.length - 1;
+  str = str.toUpperCase();
+  while (i < j) {
+    if (!isAlphaNumeric(str.charAt(i))) i++;
+    else if (!isAlphaNumeric(str.charAt(j))) j--;
+    else if (str.charAt(i) !== str.charAt(j)) return false;
+    i++;
+    j--;
+  }
+  return true;
 };
 
 // Do not edit below this line
