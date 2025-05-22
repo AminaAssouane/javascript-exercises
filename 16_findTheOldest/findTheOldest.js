c65612cf33e86e574ca7fd116ca13bb968ed1d08
Amina <minassouane@gmail.com>
2025-05-22 09:01:39 +0100

    Implement solution for findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const currentYear = new Date().getFullYear();
+  return people.reduce((oldest, current) => {
+    let ageOldest;
+    let ageCurrent;
+    if ("yearOfDeath" in oldest)
+      ageOldest = oldest.yearOfDeath - oldest.yearOfBirth;
+    else ageOldest = currentYear - oldest.yearOfBirth;
+    if ("yearOfDeath" in current)
+      ageCurrent = current.yearOfDeath - current.yearOfBirth;
+    else ageCurrent = currentYear - current.yearOfBirth;
 
+    if (ageOldest < ageCurrent) return current;
+    else return oldest;
+  }, people[0]);
 };
 
 // Do not edit below this line
